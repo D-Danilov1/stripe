bf419cf26e1d9f238a73b954819f77674156280a
HODOR72 <72arkhipov@mail.ru>
2023-07-01 20:29:18 +0500

    hlep

--- src/pages/Subscribe/Subscribe.tsx
+++ src/pages/Subscribe/Subscribe.tsx
@@ -46,7 +46,7 @@ const Subscribe = (): JSX.Element => {
 		const stripe: Stripe | null | any = await stripePromise
 		// @ts-ignore
 		if (!stripe) return null
-
+		console.log('help')
 		// const { error } = await stripe.confirmPayment({
 		// 	elements: stripeElements,
 		// 	confirmParams: {
